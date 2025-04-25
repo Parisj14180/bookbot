9e4a2ec6f332de321879568f267a3eb3d248b677
Parisj14180 <paris.jones14180@gmail.com>
2025-04-25 17:12:09 +0000

    Made "def get_book_text" and "main()"

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(books):
+    with open(books) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+main()
